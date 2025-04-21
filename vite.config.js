1a54af36512b4d247a2019c06d4846c0f354b9ed
Eyoba_Smax <eyobsmax@gmail.com>
2025-04-21 12:07:38 +0300

    final builds

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/socket.io": {
-        target: "http://127.0.0.1:5000",
+        target: "https://eyoba.up.railway.app",
         ws: true,
       },
     },
