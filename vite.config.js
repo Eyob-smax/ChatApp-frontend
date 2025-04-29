7c01371c70155fbfba509b09308ab6d41f2e1a27
Eyoba_Smax <eyobsmax@gmail.com>
2025-04-30 00:10:52 +0300

    add database to the project

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/socket": {
+        target: "https://eyoba.up.railway.app/",
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
 });
