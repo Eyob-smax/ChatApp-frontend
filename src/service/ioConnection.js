a0771006b409ace95b3d68e2ca0311e692110a15
Eyoba_Smax <eyobsmax@gmail.com>
2025-05-17 14:07:03 +0300

    final touches

--- src/service/ioConnection.js
+++ src/service/ioConnection.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://eyoba.up.railway.app", {
+const socket = io("https://eyoba.up.railway.app/", {
   transports: ["websocket"],
 });
 export default socket;
